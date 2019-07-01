7b894d45f3677ba707fd950b6084d6c640e1caa8
Sivaram Balakrishnan <sivaram@iyangi.com>
2019-07-01 15:36:58 +0530

    init vanilla todo

---
+++ vanilla-todo/main.js
@@ -0,0 +1,3 @@
+import { createList } from './modules/lists.js'
+
+let memeL = createList("meme", "meme is not kill", "label MEmE")
---
+++ vanilla-todo/modules/lists.js
@@ -0,0 +1,13 @@
+let createList = (name, about, label) => {
+  return {
+    name: name,
+    about: about,
+    label: label
+  }
+}
+
+function renderList(list) {
+  return 
+}
+
+export { createList }
