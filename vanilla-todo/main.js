538a0927898e1063bdcde984f15b90e125f9d17d
Sivaram Balakrishnan <sivaram@iyangi.com>
2019-07-09 12:37:41 +0530

    implement adding items to indexedDB

--- vanilla-todo/main.js
+++ vanilla-todo/main.js
@@ -15,7 +15,6 @@ function initDb (name) {
       let store = db.createObjectStore('lists', { keyPath: 'listId' })
       let nameIndex = store.createIndex('by_name', 'name')
       let labelIndex = store.createIndex('by_label', 'label')
-      console.log(db)
     }
     request.onsuccess = e => {
       db = e.target.result
@@ -44,26 +43,56 @@ function insertList (list) {
     let tx = db.transaction('lists', 'readwrite')
     let store = tx.objectStore('lists')
     store.put(list)
-  }
-  )
+  })
 }
 
-function fetchByListId (listId) {
+function fetchListById (listId) {
   let dbProm = initDb('VanillaToDo')
   return dbProm.then(e => new Promise((resolve, reject) => {
     let tx = db.transaction('lists', 'readonly')
     let store = tx.objectStore('lists')
-    let result = store.get(listId)
+    let req = store.get(listId)
     tx.oncomplete = e => {
-      resolve(result)
-
-      console.log('mememe')
+      resolve(req)
     }
   }))
 }
 
-insertList(dLists[0])
+function deleteListById (listId) {
+  let dbProm = initDb('VanillaToDo')
+  return dbProm.then(e => new Promise((resolve, reject) => {
+    let tx = db.transaction('lists', 'readwrite')
+    let store = tx.objectStore('lists')
+    let req = store.delete(listId)
+    tx.oncomplete = e => {
+      resolve(req)
+    }
+  }))
+}
 
-console.log(fetchByListId('010').then(x => console.log(x.result)))
+function addItemToList (item, listId) {
+  let dbProm = initDb('VanillaToDo')
+  return dbProm.then(e => new Promise((resolve, reject) => {
+    fetchListById(listId).then(req => {
+      let listR = req.result
+      let items = listR['items']
+      items.push(item)
+      listR.items = items
+      let tx = db.transaction('lists', 'readwrite')
+      let store = tx.objectStore('lists')
+      store.put(listR)
+      tx.oncomplete = e => {
+        resolve()
+      }
+    })
+  }))
+}
+
+insertList(dLists[0])
+insertList(dLists[1])
+
+console.log(fetchListById('010').then(req => console.log(req.result)))
+console.log(deleteListById('010').then(req => console.log(req)))
+addItemToList('mok', '110').then(() => console.log('keel'))
 
 getDemoData().forEach(x => renderListDesc(x))
